0727237fc37a83e4551f696ee1c438e255dfc47e
jaum537 <joaobarbosa5337@gmail.com>
2023-11-17 15:25:53 -0300

    Criação do CSS + Inicio do Jquery

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#listaeventonovo").click(function(){
+        var listaeventonovo = $("#adicionarnovoevento").val();
+        if (listaeventonovo != ""){
+            
+        }
+    })
+})

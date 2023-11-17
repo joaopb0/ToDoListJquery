b8bd5baa63910d38ba4b0f7cf92f305808eaf89f
jaum537 <joaobarbosa5337@gmail.com>
2023-11-17 16:10:59 -0300

    Tarefas

--- js/script.js
+++ js/script.js
@@ -1,15 +1,9 @@
 $(document).ready(function() {
-    // Adiciona um evento de clique ao botão "Adicionar"
     $("#adicionartarefa").click(function() {
-        // Obtém o valor do campo de entrada
         var novaTarefa = $("#novatarefa").val();
-
-        // Verifica se o campo não está vazio
         if (novaTarefa !== "") {
-            // Adiciona um novo item no início da lista
-            $("#lista").prepend('<li class="list-group-item">' + novaTarefa + '</li>');
-
-            // Limpa o campo de entrada
+            $("#instrucao").remove();
+            $("#lista").append('<li class="list-group-item">' + novaTarefa + '</li>');
             $("#novatarefa").val("");
         }
     });

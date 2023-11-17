538b7be051935142f5d3c85e0aaac84d534a06b2
jaum537 <joaobarbosa5337@gmail.com>
2023-11-17 16:05:43 -0300

    Criação da ação de adicionar

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
-$(document).ready(function(){
-    $("#listaeventonovo").click(function(){
-        var listaeventonovo = $("#adicionarnovoevento").val();
-        if (listaeventonovo != ""){
-            
+$(document).ready(function() {
+    // Adiciona um evento de clique ao botão "Adicionar"
+    $("#adicionartarefa").click(function() {
+        // Obtém o valor do campo de entrada
+        var novaTarefa = $("#novatarefa").val();
+
+        // Verifica se o campo não está vazio
+        if (novaTarefa !== "") {
+            // Adiciona um novo item no início da lista
+            $("#lista").prepend('<li class="list-group-item">' + novaTarefa + '</li>');
+
+            // Limpa o campo de entrada
+            $("#novatarefa").val("");
         }
-    })
-})
+    });
+});

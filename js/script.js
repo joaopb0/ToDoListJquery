c1f5b5ebaf23eebcc630cb08bf1dac7ec77fcd55
jaum537 <joaobarbosa5337@gmail.com>
2023-11-21 01:10:41 -0300

    Conclusão da lista

--- js/script.js
+++ js/script.js
@@ -3,8 +3,16 @@ $(document).ready(function() {
         var novaTarefa = $("#novatarefa").val();
         if (novaTarefa !== "") {
             $("#instrucao").remove();
-            $("#lista").append('<li class="list-group-item">' + novaTarefa + '</li>');
+            $("#lista").append('<li class="list-group-item textarea">' + novaTarefa + '<button type="button" class="btn btn-danger btn-sm float-end deletar">Deletar</button></li>');
             $("#novatarefa").val("");
         }
+    });
+
+    $('ul').on('click','.btn-danger',function(){
+        $(this).parent('li').fadeOut();
+    });
+
+    $('ul').on('click', '.textarea', function() {
+        $(this).toggleClass('concluir');
     });
 });
